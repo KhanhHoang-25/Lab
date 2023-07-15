48af99cdec07adc1ce66555609bb65438d353c7c
Uyen Nguyen <uyennhkse160055@fpt.edu.vn>
2023-07-15 13:38:19 +0700

    Lab 2 done

--- uyenNHK-SE160055-Lab/src/components/Films.jsx
+++ uyenNHK-SE160055-Lab/src/components/Films.jsx
@@ -4,6 +4,12 @@ import { ThemeContext } from "./ThemeContext";
 
 function Films() {
   const { theme } = React.useContext(ThemeContext);
+  const [detail, setDetail] = React.useState(null);
+
+  const handleOnClick = (data) => {
+    setDetail(data);
+  };
+
   return (
     <div id="container" style={{ backgroundColor: theme.backgroundColorHTML }}>
       {listOfFilms.map((film, index) => (
@@ -14,13 +20,30 @@ function Films() {
           </p>
           <p>{film.nation}</p>
           <button>
-            <a href="#popup1" id="openPopUp">
+            <a
+              href="#popup1"
+              id="openPopUp"
+              onClick={() => handleOnClick(film)}
+            >
               {" "}
               Detail{" "}
             </a>
           </button>
         </div>
       ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <a className="close" href="#">
+            &times;
+          </a>
+          <img src={detail.img} alt="" style={{ height: 250 }} />
+          <h2>{detail.title}</h2>
+
+          <div className="content">
+            {detail.nation} - {detail.year}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
