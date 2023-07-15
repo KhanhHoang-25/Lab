4f9a56122f1bc083d7be1ece135d6e88c590ceb7
Uyen Nguyen <uyennhkse160055@fpt.edu.vn>
2023-07-15 10:39:15 +0700

    Lab 02

--- uyenNHK-SE160055-Lab/src/App.jsx
+++ uyenNHK-SE160055-Lab/src/App.jsx
@@ -2,13 +2,17 @@ import "./App.css";
 import Films from "./components/Films";
 import Footer from "./components/Footer";
 import Navigation from "./components/Nav";
+import { useContext } from "react";
+import { ThemeContext } from "./components/ThemeContext";
+
 function App() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <>
+    <div className="App" style={{ backgroundColor: theme.backgroundColorAll }}>
       <Navigation />
       <Films />
       <Footer />
-    </>
+    </div>
   );
 }
 
--- uyenNHK-SE160055-Lab/src/components/Nav.jsx
+++ uyenNHK-SE160055-Lab/src/components/Nav.jsx
@@ -1,21 +1,41 @@
+import { useContext } from "react";
+import { ThemeContext } from "./ThemeContext";
+
 export default function Navigation() {
+  const { theme, dark, toggle } = useContext(ThemeContext);
+
   return (
     <div id="nav">
-      <nav>
+      <nav
+        style={{
+          backgroundColor: theme.backgroundColor,
+        }}
+      >
         <ul className="ulnav">
           <li>
-            <a className="active" href="#home">
+            <a className="active" href="#home" style={{ color: theme.color }}>
               Home
             </a>
           </li>
           <li>
-            <a href="#news">News</a>
+            <a href="#news" style={{ color: theme.color }}>
+              News
+            </a>
           </li>
           <li>
-            <a href="#about">About</a>
+            <a href="#about" style={{ color: theme.color }}>
+              About
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact</a>
+            <a href="#contact" style={{ color: theme.color }}>
+              Contact
+            </a>
+          </li>
+          <li>
+            <a href="#" onClick={toggle} style={{ color: theme.color }}>
+              Switch {dark ? "light" : "dark"} mode
+            </a>
           </li>
         </ul>
       </nav>
